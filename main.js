92271f31cf0d82d2e34d06029e4e310dbc39378f
Gina Häußge <gina@octoprint.org>
2020-07-07 11:18:44 +0200

    Make prerelease inclusion optional

--- main.js
+++ main.js
@@ -4,23 +4,32 @@ const { Octokit } = require("@octokit/rest");
 const repository = core.getInput('repository');
 var owner = core.getInput('owner');
 var repo = core.getInput('repo');
+var prerelease = core.getInput('prerelease');
 
 const octokit = new Octokit()
 
 async function run() {
     try {
         if (repository){
-                [owner, repo] = repository.split("/");
-            }
-        const releases  = await octokit.repos.listReleases({
-            owner: owner,
-            repo: repo,
+            [owner, repo] = repository.split("/");
+        }
+
+        if (prerelease) {
+            const releases  = await octokit.repos.listReleases({
+                owner: owner,
+                repo: repo,
             });
-        core.setOutput('release', releases.data[0].tag_name)
+            core.setOutput('release', releases.data[0].tag_name)
+        } else {
+            const release = await octokit.repos.getLatestRelease({
+                owner: owner,
+                repo: repo,
+            });
+            core.setOutput('release', release.tag_name);
         }
-    catch (error) {
+    } catch (error) {
         core.setFailed(error.message);
-        }
     }
+}
 
 run()
